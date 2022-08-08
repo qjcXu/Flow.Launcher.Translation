ad75ddc065fd06bb27dc60a710cc855f2a3ddcb9
ming <mingzhi27@outlook.com>
2022-08-08 21:16:42 +0800

    feat(config): 增加百度翻译的appid和key

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  IcoPath: './src/assets/imgs/translate.png',
+  DocUrl: 'https://github.com/qjcXu/Flow.Launcher.Transition#readme',
+  appid: '百度翻译appid',
+  key: '百度翻译key'
+}
