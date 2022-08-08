df780470c27c2d34361a2a3d323495e83915cd5e
ming <mingzhi27@outlook.com>
2022-08-08 21:23:19 +0800

    test(test): 测试查询模块

---
+++ test/query.test.js
@@ -0,0 +1,56 @@
+import { config } from '../src/config/config.js'
+import { translate } from '../src/js/translate.js'
+
+function query(parameters) {
+  const argsZ = parameters.trim().match(/^z (\w+)$/)
+  const argsE = parameters.trim().match(/^e ([\u4e00-\u9fa5]+)$/)
+  if (argsZ !== null || argsE !== null) {
+    const args = argsZ === null ? argsE : argsZ
+    const to = args[0].charAt(0) === 'e' ? 'en' : 'zh'
+    const from = to === 'en' ? 'zh' : 'en'
+    translate(from, to, args[1], (res) => {
+      success([{
+        Title: res.trans_result[0].src + ' ==> ' + res.trans_result[0].dst,
+        IcoPath: config.IcoPath,
+        score: 100,
+      }])
+    })
+  }
+  return err([{
+    Title: 'enter z or e',
+    SubTitle: 'not support one of languages which you entered',
+    IcoPath: config.IcoPath,
+    score: 100,
+  }])
+}
+
+query('z apple')
+
+function success(result = []) {
+  try {
+    console.log(JSON.stringify({ result }));
+  } catch {
+    err([{
+      Title: 're-enter',
+      IcoPath: config.IcoPath,
+      score: 100,
+    }])
+  }
+}
+
+function err(result = []) {
+  console.log(JSON.stringify({ result }));
+}
+
+function getHelpResult() {
+	return {
+		Title: '13',
+		SubTitle: "Click to open documentation",
+		IcoPath: config.IcoPath,
+		jsonRPCAction: {
+			method: "open_in_url",
+			parameters: [config.DocUrl],
+		},
+		score: 0,
+	}
+}
