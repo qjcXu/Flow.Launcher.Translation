edda225a9ad87a0c7cf1b7cef022b697cf648ddb
ming <mingzhi27@outlook.com>
2022-08-09 12:02:50 +0800

    feat(src): 增加中英句子翻译

--- src/js/query.js
+++ src/js/query.js
@@ -8,8 +8,8 @@ export function query(parameters) {
     const args = argsZ === null ? argsE : argsZ
     const to = args[0].charAt(0) === 'e' ? 'en' : 'zh'
     const from = to === 'en' ? 'zh' : 'en'
-
-    return translate(from, to, args[1], res => {
+    const q = args.input.slice(2)
+    return translate(from, to, q, res => {
       try {
         success([{
           Title: res.trans_result[0].src + ' => ' + res.trans_result[0].dst,
