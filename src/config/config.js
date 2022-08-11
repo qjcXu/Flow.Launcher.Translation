49b3e987797bd6a30eb5e055678af7ef3b355789
ming <mingzhi27@outlook.com>
2022-08-11 16:41:36 +0800

    feat(config): 增加了一系列常用配置 - 存储时间文件的路径 - 请求间隔时间 - 百度翻译api接口根路径

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,9 @@
 export const config = {
-  IcoPath: './src/assets/imgs/translate.png',
-  DocUrl: 'https://github.com/qjcXu/Flow.Launcher.Transition#readme',
-  appid: '百度翻译appid',
-  key: '百度翻译key'
+  IcoPath: '/assets/imgs/translate.png',
+  timePath: '/assets/time.txt',
+  intervalTime: 2, // 秒 s 建议 2秒
+  DocUrl: 'https://github.com/qjcXu/Flow.Launcher.Translation#readme',
+  appid: '20210928000958862',
+  key: '4eKZI7cMc8y7wf75vNtD',
+  transUrl: 'http://api.fanyi.baidu.com/api/trans/vip/translate'
 }
