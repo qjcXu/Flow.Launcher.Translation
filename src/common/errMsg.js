4c7b1471a600375df9a3898bbd55399cb1833bbc
qjcXu <3162002031@qq.com>
2022-10-20 01:21:29 +0800

    update: 修改了appid未找到的变量名

--- src/common/errMsg.js
+++ src/common/errMsg.js
@@ -5,7 +5,7 @@ export const errMsg = {
   slowE: '请减慢输入速度~~',
   notSQE: `不支持快速且连续输入, 请等待${config.intervalTime}秒以上`,
   notSL: '不支持你输入的语言',
-  reEC: '请检查config文件',
+  reEC: '请检查config.js文件',
   network: '网络连接错误~~',
-  appidNotFound: '百度翻译appid或key不应为空',
+  appidOrKeyNotFound: '百度翻译appid或key不应为空',
 }
