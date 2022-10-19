08526649a12ec6eb5af9900da009d909db7521ab
qjcXu <3162002031@qq.com>
2022-10-20 01:21:57 +0800

    fixed: 修复了无法正常请求api接口的bug

--- src/js/generateParams.js
+++ src/js/generateParams.js
@@ -1,15 +1,17 @@
 import { md5 } from '../utils/md5.js'
 import { config } from '../config/config.js'
-import { toUTF8 } from '../utils/toUTF8.js'
+
 export function generateParams(query) {
-  const salt = (new Date).getTime()
-  const str1 = config.appid + query + salt + config.key
+  const salt = new Date().getTime()
+  const str = config.appid + query + salt + config.key
+  query = encodeURI(query)
+  const sign = md5(str)
   return {
-    query: toUTF8(query),
+    query: query,
     appid: config.appid,
     key: config.key,
-    salt: (new Date).getTime(),
-    str1: str1,
-    sign: md5(str1)
+    salt: salt,
+    str: str,
+    sign: sign
   }
 }
