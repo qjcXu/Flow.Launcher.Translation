153c2abb2066c0711ca919d58649c578ac929870
qjcXu <3162002031@qq.com>
2022-10-20 01:24:46 +0800

    test: 测试请求

--- test/query.test.js
+++ test/query.test.js
@@ -1,5 +1,8 @@
 import { query } from '../src/js/query.js'
-import { fileURLToPath } from 'url'
-import path from 'path'
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-query('z english', __dirname)
+
+
+
+query('en zh hello')
+
+
+
