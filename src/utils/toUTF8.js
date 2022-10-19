9c64e3af078a306284a0ff0e036b82713475a213
qjcXu <3162002031@qq.com>
2022-10-20 01:10:08 +0800

    delete

--- src/utils/toUTF8.js
+++
@@ -1,9 +0,0 @@
-
-export function toUTF8(data) {
-    const bytes = new TextEncoder('utf-8').encode(data)
-    let result = ''
-    for(var i = 0; i < bytes.length; ++i) {
-        result += String.fromCharCode(bytes[i]);
-    }
-    return result
-}
