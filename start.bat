3a542d7e8f4434d22f1c532dda6d3de9cd183ad8
qjcXu <3162002031@qq.com>
2022-10-20 01:25:27 +0800

    feat: 将版本提升到1.0.0

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 SET plugin_dir=%~dp0%
-SET icon=%plugin_dir%src\assets\imgs\translate.png
+SET icon=%plugin_dir%src\assets\images\app.png
 SET "icon=%icon:\=/%"
 SET title="Error: Node.js is not installed on your system."
 SET subtitle="Please install it from nodejs.org to use the translate plugin!"
