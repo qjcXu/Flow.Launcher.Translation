ae96d366af721ebbb7c793a46e338061f7817748
qjcXu <3162002031@qq.com>
2022-10-20 01:20:56 +0800

    delete: 删除了记录时间的文件

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,8 @@
 export const config = {
-  IcoPath: '/assets/imgs/translate.png',
-  timePath: '/assets/time.txt',
+  IcoPath: '/assets/images/app.png',
   intervalTime: 2, // 秒 s 建议 2秒
   DocUrl: 'https://github.com/qjcXu/Flow.Launcher.Translation#readme',
   appid: '', // 百度翻译appid
   key: '', // 百度翻译key
-  transUrl: 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+  transUrl: 'https://fanyi-api.baidu.com/api/trans/vip/translate'
 }
