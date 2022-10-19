0714d622142ef093b0e5eff9ba3445c32c28c9b0
qjcXu <3162002031@qq.com>
2022-10-20 01:23:44 +0800

    feat: 新增支持多种语言互译的功能 - 新增自动识别源语言的功能

--- src/js/query.js
+++ src/js/query.js
@@ -1,71 +1,63 @@
 import { config } from '../config/config.js'
 import { errMsg } from '../common/errMsg.js'
-import { generateParams } from '../js/generateParams.js'
-import { timeIO } from './timeIO.js'
+import { generateParams } from './generateParams.js'
+import { fileURLToPath } from 'url'
 import path from 'path'
 import axios from 'axios'
-
-export async function query(parameters, fPath) {
-  const argsZ = parameters.trim().match(/^z (\w+)/)
-  const argsE = parameters.trim().match(/^e ([\u4e00-\u9fa5]+)/)
-  const iconPath = path.join(fPath, config.IcoPath)
-  if (argsZ !== null || argsE !== null) {
-    if (config.appid === '' || config.key === '') {
-      return err(returnC(errMsg.appidNotFound, errMsg.reEC, iconPath, null, 100), iconPath)
-    }
-    const timePath = path.join(fPath, config.timePath)
-    const lastTime = timeIO(timePath, true)
-    if (!isPermit(lastTime, timePath)) {
-      return err(returnC(errMsg.slowE, errMsg.notSQE, iconPath, null, 100), iconPath)
-    }
-    const args = argsZ === null ? argsE : argsZ
-    const query = args.input.slice(2)
-    const params = generateParams(query)
-    const to = args[0].charAt(0) === 'e' ? 'en' : 'zh'
-    const from = to === 'en' ? 'zh' : 'en'
-    const { data: res } = await axios.get(
-      `${config.transUrl}?q=${params.query}&appid=${params.appid}&salt=${params.salt}&from=${from}&to=${to}&sign=${params.sign}`
-    ).catch(e => {
-      return err(returnC(errMsg.network, errMsg.reEC, iconPath, null, 100), iconPath)
-    })
-    if (res.trans_result) {
-      const [content] = res.trans_result
-      return success(returnC(content.dst, from + '=>' + to + ' ' + content.src, iconPath, null, 100))
-    }
-    return err(returnC(errMsg.slowE, errMsg.notSQE, iconPath, null, 100), iconPath)
+const __dirname = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '..')
+const iconPath = path.join(__dirname, config.IcoPath)
+export async function query(parameters) {
+  parameters = parameters.trim()
+  const firstArgEnd = parameters.indexOf(' ')
+  const from = parameters.substring(0, firstArgEnd)
+  const secondArgEnd = parameters.indexOf(' ', firstArgEnd + 1)
+  const to = parameters.substring(firstArgEnd + 1, secondArgEnd)
+  const params = generateParams(parameters.substring(secondArgEnd + 1))
+  const { data: res} = await axios.get(
+  `${config.transUrl}?q=${params.query}&appid=${params.appid}&salt=${params.salt}&from=${from}&to=${to}&sign=${params.sign}`
+  )
+  if (!res.trans_result) {
+    return catchErr(res.error_code)
   }
-  return err(returnC(errMsg.reE, errMsg.notSL, iconPath, null, 100), iconPath)
+  const [content] = res.trans_result
+  return success(returnC(content.src, from + ' => ' + to + ' ' + content.dst,100))
 }
 
 function success(result = []) {
   console.log(JSON.stringify({ result }));
 }
 
-function err(result = [], iconPath) {
-  result.push(returnC('打开文档', 'github doc', iconPath, {
+function err(result = []) {
+  result.push(returnC('打开文档', 'github doc', 100, {
     method: "open_url",
     parameters: [config.DocUrl]
   })[0]);
   console.log(JSON.stringify({ result }));
 }
 
-function returnC(title, subTitle, iconPath, jsonRPCAction, score) {
-  score ? score : 0
+function returnC(title, subTitle, score, jsonRPCAction) {
   return [{
     Title: title,
-		SubTitle: subTitle,
-		IcoPath: iconPath,
-		jsonRPCAction: jsonRPCAction,
-		score: score
+    SubTitle: subTitle,
+    IcoPath: iconPath,
+    jsonRPCAction: jsonRPCAction,
+    score: score
   }]
 }
 
-function isPermit(lastTime, timePath) {
-  const nowTime = new Date().getSeconds()
-  const intervalTime = nowTime < lastTime ? 60 - lastTime + nowTime : nowTime - lastTime
-  if (intervalTime > config.intervalTime) {
-    timeIO(timePath, false, nowTime)
-    return true
+function catchErr(errCode) {
+  switch (errCode) {
+    case '52001':
+    case '52002':
+    case '52003':
+    case '54001':
+    case '90107':
+      return err(returnC(errMsg.appidOrKeyNotFound, errMsg.reEC, 100))
+    case '54000':
+    case '58001':
+      return err(returnC(errMsg.notSL, errMsg.reE, 100))
+    case '54003':
+    case '54005':
+      return err(returnC(errMsg.notSQE, errMsg.slowE, 100))
   }
-  return false
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { query } from './js/query.js'
-import { fileURLToPath } from 'url'
 import open from 'open'
-import path from 'path'
 const { method, parameters } = JSON.parse(process.argv[2])
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 if (method === 'query') {
-	query(parameters[0], __dirname)
+	query(parameters[0])
 }
 
 if (method === 'open_url') {
