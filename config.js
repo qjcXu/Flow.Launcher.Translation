92492ede31183bd43f69443dd338a8101f2b3d7b
qjcXu <3162002031@qq.com>
2023-03-06 18:41:23 +0800

    feat: 新增常用语言的设置

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ export const config = {
   IcoPath: './assets/images/app.png',
   intervalTime: 2, // 秒 s 建议 2秒
   DocUrl: 'https://github.com/qjcXu/Flow.Launcher.Translation#readme',
-  appid: '20210928000958862', // 百度翻译appid
-  key: '4eKZI7cMc8y7wf75vNtD', // 百度翻译key
+  appid: '', // 百度翻译appid
+  key: '', // 百度翻译key
   transUrl: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
   fu: 'en' // 常用语言
 }
