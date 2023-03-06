121cd9cd44ee46a528da7d268f1cdcce472ab85b
qjcXu <3162002031@qq.com>
2023-03-06 18:36:00 +0800

    del: 已将文件移动到主目录中

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  IcoPath: '/assets/images/app.png',
-  intervalTime: 2, // 秒 s 建议 2秒
-  DocUrl: 'https://github.com/qjcXu/Flow.Launcher.Translation#readme',
-  appid: '', // 百度翻译appid
-  key: '', // 百度翻译key
-  transUrl: 'https://fanyi-api.baidu.com/api/trans/vip/translate'
-}
