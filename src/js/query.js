2cb7a27da07083f4bfeb0db3885126470ca545fd
qjcXu <3162002031@qq.com>
2023-05-19 21:48:20 +0800

    refactor: 重构代码，减少操作误判

--- src/js/query.js
+++ src/js/query.js
@@ -7,49 +7,65 @@ import axios from 'axios'
 const __dirname = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '..')
 const iconPath = path.join(__dirname, config.iconPath)
 
+// 配置参数
+const configParams = ['appid', 'key', 'dest', 'delay']
+
 export async function query(parameters) {
   parameters = parameters.trim()
-  // 配置修改
-  if ('appid' === parameters || 'key' === parameters || 'dest' === parameters) {
-    return success(returnParam(parameters + ' ' + config[parameters], parameters + '的配置信息如上', 100))
+
+  let commond = parameters.toLowerCase()
+  // 显示配置
+  if (configParams.includes(commond)) {
+    return success(returnParam(commond + ' ' + config[commond], commond + '的配置信息如上', 100))
   }
 
-  const argEnd = parameters.indexOf(' ')
-  const firstParam = -1 === argEnd ? parameters : parameters.substring(0, argEnd)
-  if ('appid' === firstParam || 'key' === firstParam || 'dest' === firstParam) {
-    const settingParam = -1 === argEnd ? '' : parameters.substring(argEnd + 1)
-    return success(returnParam(firstParam + ' ' + settingParam, '点击此项或者回车修改', 100, {
+  const splitParams = parameters.split(' ')
+
+  // 修改配置
+  commond = splitParams[0].toLowerCase()
+  if (splitParams.length === 2 && configParams.includes(commond)) {
+    return success(returnParam(commond + ' ' + splitParams[1], '点击此项或者回车修改', 100, {
       method: 'modify',
-      parameters: [firstParam, settingParam]
+      parameters: [commond, splitParams[1]]
     }))
   }
-  // 正常翻译
-  let params
-  params = -1 === argEnd ? generateParams(parameters) : generateParams(parameters.substring(parameters.indexOf(' ') + 1))
-  const to = -1 === argEnd ? config.dest : parameters.substring(0, parameters.indexOf(' '))
 
+  // 解析参数
+  const to = languages.includes(commond) ? commond : config.dest
+  const params = languages.includes(commond) ? generateParams(parameters.substring(parameters.indexOf(' ') + 1)) : generateParams(parameters)
+
+  // 没有输入命令或者单词（防止无效请求）
+  if (parameters === '' || params.source === to) {
+    return err(returnParam(message.parameterNotFound, message.parameterNotFoundSubTitle, 100))
+  }
+
+  // 正常翻译
   const { data: res} = await axios.get(
-  `${config.apiUrl}?q=${params.query}&appid=${params.appid}&salt=${params.salt}&from=auto&to=${to}&sign=${params.sign}`
+    `${config.apiUrl}?q=${params.query}&appid=${params.appid}&salt=${params.salt}&from=auto&to=${to}&sign=${params.sign}`
   )
   if (!res.trans_result) {
     return catchErr(res.error_code)
   }
   const [content] = res.trans_result
-  return success(returnParam(content.dst, 'auto' +  ' => ' + config.dest + '   ' + content.src + ' => ' + content.dst, 100, {
+  return success(returnParam(content.dst, 'auto' +  ' => ' + to + '   ' + content.src + ' => ' + content.dst, 100, {
     method: 'copy',
     parameters: [content.dst]
   }))
 }
 
 function success(result = []) {
+  result.push(returnParam(message.tips, message.tipsSubTitle, 100, {
+    method: 'open_url',
+    parameters: [config.docUrl]
+  })[0])
   console.log(JSON.stringify({ result }));
 }
 
 function err(result = []) {
-  result.push(returnParam('打开文档', 'github doc (⭐翻译结果可点击复制到剪切板)', 100, {
+  result.push(returnParam(message.openDoc, message.openDocSubTitle, 100, {
     method: 'open_url',
     parameters: [config.docUrl]
-  })[0]);
+  })[0])
   console.log(JSON.stringify({ result }));
 }
 
@@ -71,7 +87,7 @@ function catchErr(errCode) {
     case '52003':
     case '54001':
     case '90107':
-      return err(returnParam(message.appidOrKeyNotFound, message.reEC, 100))
+      return err(returnParam(message.appidOrKeyNotFound, message.appidOrKeyNotFoundSubTitle, 100))
     case '54000':
     case '58001':
       return err(returnParam(message.notSL, message.reE, 100))
@@ -80,3 +96,10 @@ function catchErr(errCode) {
       return err(returnParam(message.notSQE, message.slowE, 100))
   }
 }
+
+const languages = [
+  'zh', 'kor', 'fra', 'spa', 'th', 'ara', 'ru', 'pt',
+  'de', 'it', 'el', 'nl', 'pl', 'bul', 'est', 'dan',
+  'fin', 'cs', 'rom', 'slo', 'swe', 'hu', 'cht', 'vie',
+  'jp', 'yue', 'en'
+]
